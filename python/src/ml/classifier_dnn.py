05d8148daaa71ea80d9b6590d529849652347ae9
zz <ziqizhang.email@gmail.com>
2017-07-19 19:37:52 +0100

    dnn

--- python/src/ml/classifier_dnn.py
+++ python/src/ml/classifier_dnn.py
@@ -1,7 +1,9 @@
 import datetime
+import logging
 import os
 import sys
 
+import functools
 import pandas as pd
 import pickle
 from keras.layers import Dense, Embedding, Conv1D, MaxPooling1D, LSTM
@@ -9,17 +11,19 @@ from keras.models import Sequential
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.cross_validation import cross_val_predict, train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import f_classif
 from sklearn.model_selection import GridSearchCV
+from keras.preprocessing import sequence
 
 from ml import util
-#from util import logger
 from ml import nlp
 from ml import text_preprocess as tp
 
+WORD_EMBEDDING_DIM=100
+EMBEDDING_LAYER_MAX_INDEX=900
+logger = logging.getLogger(__name__)
+LOG_DIR=os.getcwd()+"/logs"
+logging.basicConfig(filename=LOG_DIR+'/dnn-log.txt', level=logging.INFO, filemode='w')
 
-INPUT_DIM=1000
 def get_dnn_wordembedding_input(tweets, out_folder):
     word_vectorizer = CountVectorizer(
             # vectorizer = sklearn.feature_extraction.text.CountVectorizer(
@@ -33,37 +37,39 @@ def get_dnn_wordembedding_input(tweets, out_folder):
             max_df=0.501
         )
 
-    #logger.logger.info("\tgenerating word vectors, {}".format(datetime.datetime.now()))
+    logger.info("\tgenerating word vectors, {}".format(datetime.datetime.now()))
     counts = word_vectorizer.fit_transform(tweets).toarray()
-    #logger.logger.info("\t\t complete, dim={}, {}".format(counts.shape, datetime.datetime.now()))
+    logger.info("\t\t complete, dim={}, {}".format(counts.shape, datetime.datetime.now()))
     vocab = {v: i for i, v in enumerate(word_vectorizer.get_feature_names())}
     pickle.dump(vocab, open(out_folder+"/"+"DNN_WORD_EMBEDDING"+".pk", "wb" ))
 
     word_embedding_input=[]
     for tweet in counts:
+        tweet_vocab=[]
         for i in range(0, len(tweet)):
             if tweet[i]!=0:
-                word_embedding_input.append(i)
+                tweet_vocab.append(i)
+        word_embedding_input.append(tweet_vocab)
     return word_embedding_input, vocab
 
 
 def learn_dnn(cpus, nfold, task, load_model,X_train, y_train, X_test, y_test,
               identifier, outfolder):
-    #logger.logger.info("== Perform ANN ...")
+    logger.info("== Perform ANN ...")
     subfolder=outfolder+"/models"
     try:
         os.stat(subfolder)
     except:
         os.mkdir(subfolder)
-    model = KerasClassifier(build_fn=create_model, verbose=0)
-    # define the grid search parameters
-    batch_size = [10]
-    epochs = [100]
-    dropout = [0.3]
-    fs=SelectKBest(k=INPUT_DIM, score_func=f_classif)
-    param_grid = dict(dropout_rate=dropout, batch_size=batch_size, nb_epoch=epochs)
 
-    _X_train=fs.fit_transform(X_train, y_train)
+    create_model_with_args=functools.partial(create_model, max_index=EMBEDDING_LAYER_MAX_INDEX)
+    model = KerasClassifier(build_fn=create_model_with_args, verbose=0)
+    # define the grid search parameters
+    cv_output_dim=[32]
+    batch_size = [50]
+    epochs = [3]
+    param_grid = dict(batch_size=batch_size, nb_epoch=epochs, output_dim=cv_output_dim)
+
     _classifier =  GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=cpus,
                         cv=nfold)
 
@@ -73,35 +79,34 @@ def learn_dnn(cpus, nfold, task, load_model,X_train, y_train, X_test, y_test,
     nfold_predictions=None
 
     if load_model:
-        #logger.logger.info("model is loaded from [%s]" % str(ann_model_file))
+        logger.info("model is loaded from [%s]" % str(ann_model_file))
         best_estimator = util.load_classifier_model(ann_model_file)
     else:
-        _classifier.fit(_X_train, y_train)
-        nfold_predictions=cross_val_predict(_classifier.best_estimator_, _X_train, y_train, cv=nfold)
+        _classifier.fit(X_train, y_train)
+        nfold_predictions=cross_val_predict(_classifier.best_estimator_, X_train, y_train, cv=nfold)
 
         cv_score_ann = _classifier.best_score_
         best_param_ann = _classifier.best_params_
-        #logger.logger.info("+ best params for {} model are:{}".format(model, best_param_ann))
+        logger.info("+ best params for {} model are:{}".format(model, best_param_ann))
         best_estimator = _classifier.best_estimator_
 
         #util.save_classifier_model(best_estimator, ann_model_file)
 
-    #logger.logger.info("testing on development set ....")
-    X_test=None
+    logger.info("testing on development set ....")
     if(X_test is not None):
         heldout_predictions_final = best_estimator.predict(X_test)
         util.save_scores(nfold_predictions,y_train, heldout_predictions_final, y_test, 'dnn', task,
                          identifier,2,outfolder)
 
     else:
-        util.save_scores(nfold_predictions,y_train, None, y_test, model, task, identifier,2,
+        util.save_scores(nfold_predictions,y_train, None, y_test, 'dnn', task, identifier,2,
                          outfolder)
 
     #util.print_eval_report(best_param_ann, cv_score_ann, dev_data_prediction_ann,
     #                       time_ann_predict_dev,
     #                       time_ann_train, y_test)
 
-def create_model(dropout_rate=0.5):
+def create_model(max_index=100):
     # create model
     # model = Sequential()
     # model.add(Dense(500, input_dim=1000, activation='relu'))
@@ -112,19 +117,19 @@ def create_model(dropout_rate=0.5):
 
 
     model = Sequential()
-    model.add(Embedding(input_dim=11230, output_dim=32, input_length=500))
+    model.add(Embedding(input_dim=max_index, output_dim=32, input_length=WORD_EMBEDDING_DIM))
     model.add(Conv1D(filters=32, kernel_size=4, padding='same', activation='relu'))
     model.add(MaxPooling1D(pool_size=2))
     model.add(LSTM(100))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    #logger.logger.info("New run started at {}\n{}".format(datetime.datetime.now(),model.summary()))
+    logger.info("New run started at {}\n{}".format(datetime.datetime.now(),model.summary()))
     return model
 
 
 def gridsearch(data_file, sys_out):
     raw_data = pd.read_csv(data_file, sep=',', encoding="utf-8")
-    M=get_dnn_wordembedding_input(raw_data.tweet,sys_out)
+    M=get_dnn_wordembedding_input(raw_data.tweet,sys_out)[0]
     #M=self.feature_scale(M)
 
     # split the dataset into two parts, 0.75 for train and 0.25 for testing
@@ -132,11 +137,11 @@ def gridsearch(data_file, sys_out):
                 train_test_split(M, raw_data['class'],
                                  test_size=0.25,
                                  random_state=42)
-    X_train_data=util.feature_scale(0,X_train_data)
-    X_test_data = util.feature_scale(0,X_test_data)
     y_train = y_train.astype(int)
     y_test = y_test.astype(int)
 
+    X_train_data = sequence.pad_sequences(X_train_data, maxlen=WORD_EMBEDDING_DIM)
+    X_test_data = sequence.pad_sequences(X_test_data, maxlen=WORD_EMBEDDING_DIM)
 
     learn_dnn(-1, 5, 'td-tdf', False,
                              X_train_data,
