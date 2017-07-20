949dd3414bf00c71d87a157596477b89a7c39101
zz <ziqizhang.email@gmail.com>
2017-07-20 20:33:26 +0100

    .

--- job_iceberg.sh
+++ job_iceberg.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-#$ -l h_rt=120:00:00
-# below -m option can have any combination of b , e or a  to imply when to to send email where;
-#    b = begining of job  e = end of job  a = in case job gets aborted unexpectedly 
-#$ -m be
-#$ -M ziqi.zhang@ntu.ac.uk 
-#$ -l mem= 16G
-#$ -l rmem= 14G
+#$ -l h_rt=120:00:00 -m be -M ziqi.zhang@ntu.ac.uk -l mem= 16G -l rmem= 14G
 module load apps/python/anaconda3-2.5.0
 source activate myexp
 cd chase
