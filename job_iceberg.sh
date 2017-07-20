a6154e036d462a2080313ff410c4ecaa526995d9
zz <ziqizhang.email@gmail.com>
2017-07-20 20:41:07 +0100

    .

--- job_iceberg.sh
+++ job_iceberg.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-#$ -l h_rt=120:00:00 -m be -M ziqi.zhang@ntu.ac.uk -l mem= 16G -l rmem= 14G
+#!/bin/sh
+#$ -l h_rt=120:00:00 -m bea -M ziqi.zhang@ntu.ac.uk -l mem=16G -l rmem=14G
 module load apps/python/anaconda3-2.5.0
 source activate myexp
 cd chase
