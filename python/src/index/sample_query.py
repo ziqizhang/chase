fede48ca890fa14fb33efde3a1da7c1adbcbb7a9
zz <ziqizhang.email@gmail.com>
2017-07-28 21:01:36 +0100

    results

---
+++ python/src/index/sample_query.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+'''
+Firstly start the server by:
+$ cd solr-6.6.0/bin
+$ ./solr start -s [/home/.../chase/data/solr]
+
+
+Tips for using solr server (https://cwiki.apache.org/confluence/display/solr/Running+Solr)
+- always remember TO STOP THE SERVER when you finish, by typing './solr stop -all'
+- it is better to make a back up of the index. this is located in [/home/.../chase/data/solr]
+- sometimes if server does not start, check if there is a 'write.lock' file within the
+sub folders of [/home/.../chase/data/solr]. if so, delete them and try again
+- 
+
+
+'''
